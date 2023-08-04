5f5ccd755d5b5aaad3647b40307cb0c98802ac42
Cimorexave <sadeq.s502@gmail.com>
2023-08-04 23:07:28 +0330

    creating header defs

---
+++ header/my_header.h
@@ -0,0 +1,18 @@
+// myheader.h
+
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// Function declarations
+void say_hi(void);
+
+// Data structure declaration
+struct MyStruct
+{
+    // members...
+};
+
+// Constants and macros
+#define MY_CONSTANT 69
+
+#endif // MYHEADER_H
