3ffb51fbc0abb3050c6ad4b8d203467cc66210e8
Cimorexave <sadeq.s502@gmail.com>
2023-08-05 16:23:27 +0330

    poitning void to int

--- void_pointer/void_pointer.c
+++ void_pointer/void_pointer.c
@@ -6,11 +6,29 @@ int main(int argc, char **argv)
 {
     printf("starting...\n");
 
-    void *vp;
+    // void *vp;
 
     // printf("%p\n", vp);
-    printf("%zu\n", sizeof vp);
+    // printf("%zu\n", sizeof vp); // 4
     // printf("%d\n", *vp);
+
+    // void * is basically a normal pointer
+    // just without a type
+
+    // int my_num = 0b00000001;
+    int my_num = 1;
+    int *p = &my_num;
+    void *pointer = &my_num;
+
+    // int my_num_copy = *p;
+    int my_num_copy = *(int *)pointer;
+    printf("%i\n", my_num_copy);
+
+    printf("pointer: %p\n", pointer);
+    printf("pointer: %p\n", p);
+
+    // printf("pointer value: %i\n", *(int *)my_num);
+    // printf("pointer value: %i\n", *p);
 
     return 0;
 }
