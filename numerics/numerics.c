34fd60151c2dfe1bad3b8b9dc2e306326241a3d8
Cimorexave <sadeq.s502@gmail.com>
2023-08-05 13:58:18 +0330

    ternary operators

--- numerics/numerics.c
+++ numerics/numerics.c
@@ -14,7 +14,29 @@ int main(int argc, char **argv)
     printf("%i\n", ab);
     printf("%i\n", ax);
 
-    
+    int a_negative = ~ab + 1; // two's compliment
+
+    printf("%i\n", a_negative);
+
+    int a_compliment = ~ab;
+    printf("%i\n", a_compliment);
+
+    // int a_and_a = ab && ab;
+    // int ab &= 0;
+    // printf("%i\n", ab);
+
+    printf("break\n");
+    int x = 1;
+
+    // x &= 0;
+    x |= 0;
+    // x ^= 0;
+
+    printf("%i\n", x);
+    // printf("%i\n", x |= 0);
+    // printf("%i\n", x ^= 0);
+
+    printf("break...\n");
 
     return 0;
 }
